9d07e9fa24915f7f8adb070df6fca165e9890b43
Diogo <drroessler@hotmail.com.br>
2021-05-21 18:44:55 -0300

    Implementing drag and drop

--- MEDIA PLAYER/MainWindow.xaml.cs
+++ MEDIA PLAYER/MainWindow.xaml.cs
@@ -32,6 +32,7 @@ using System.Collections;
 using System.Windows.Automation.Peers;
 using System.ComponentModel;
 using System.Threading;
+using System.IO;
 
 namespace MEDIA_PLAYER
 {
@@ -616,6 +617,47 @@ namespace MEDIA_PLAYER
 
                     counter = 0;
                 }
+            }
+        }
+
+        private void Window_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
+            {
+                string DataString = (string)e.Data.GetData(DataFormats.StringFormat, true);
+                e.Effects = DragDropEffects.Copy;
+            }
+        }
+
+        private void Window_DragLeave(object sender, DragEventArgs e)
+        {
+            return;
+        }
+
+        private void Window_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.None;
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
+            {
+                string[] DataString = (string[])e.Data.GetData(DataFormats.StringFormat, true);
+            }
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Handled)
+            {
+                if (DragDrop.DoDragDrop(this, DataFormats.FileDrop, DragDropEffects.Copy | DragDropEffects.Move) == DragDropEffects.Copy)
+                    mediaPlayerView.Play();
+            }
+        }
+
+        private void Window_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
+            {
+                string[] DataString = (string[])e.Data.GetData(DataFormats.StringFormat, false);
             }
         }
     }
